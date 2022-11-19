7cb3eca807272c3344db25486dbe9dd033429664
Kevin Wyckmans <kevinwijckmans@gmail.com>
2022-11-19 17:51:07 +0100

    Experiment with error handling

--- src/main.rs
+++ src/main.rs
@@ -3,16 +3,22 @@ use clap::Parser;
 #[derive(Parser)]
 struct Cli {
     pattern: String,
-    path: std::path::PathBuf
+    path: std::path::PathBuf,
 }
 
 fn main() {
     let args = Cli::parse();
 
-    let content = std::fs::read_to_string(&args.path).expect("could not read file");
+    let content = std::fs::read_to_string(&args.path);
+    let content = match content {
+        Ok(content) => content,
+        Err(error) => {
+            panic!("Could not read provided file {:?}", error)
+        }
+    };
 
-    for line in content.lines(){
-        if line.contains(&args.pattern){
+    for line in content.lines() {
+        if line.contains(&args.pattern) {
             println!("{}", line)
         }
     }
